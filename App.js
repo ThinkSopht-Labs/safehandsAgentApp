6c3bdf4fee2024795860eff5cf8fd2a0955a93f0
Gerald <mcamanor@gmail.com>
2020-09-09 17:05:09 +0000

    Install react-native-chart-kit

--- App.js
+++ App.js
@@ -66,7 +66,7 @@ const App = () => {
        {/* <DeliveryDetails /> */}
        {/* <ComplimentsRatings /> */}
        {/* <DeliveryAgentRatingsCard /> */}
-       <RideSummary />
+       {/* <RideSummary /> */}
        
     </>
   )
