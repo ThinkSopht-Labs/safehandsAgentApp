991b36ecc807a3361267dfe1b2339534528e165d
Gerald <mcamanor@gmail.com>
2020-10-22 15:39:03 +0000

    Fixed a form button bug (isDisabled)

--- components/buttons/FormButton.js
+++ components/buttons/FormButton.js
@@ -4,12 +4,13 @@ import { Text, TouchableOpacity, StyleSheet, ActivityIndicator } from 'react-nat
 export default class FormButton extends Component {
     render() {
         return (
-            <TouchableOpacity disabled={this.props.isLoading} onPress={this.props.handleSubmit} style={[styles.btn, this.props.style, this.props.disabled && styles.greyBg]}>
+            <TouchableOpacity disabled={this.props.disabled} onPress={this.props.handleSubmit} style={[styles.btn, this.props.style, this.props.disabled && styles.greyBg]}>
               {this.props.isLoading ? <ActivityIndicator size="small" color="#fff" /> : <Text style={styles.btnText}>{this.props.label}</Text>}
             </TouchableOpacity>
         )
     }
 }
+
 
 const styles = StyleSheet.create({
     btn: {
